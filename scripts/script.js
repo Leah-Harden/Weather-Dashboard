1382aaf4daf822fc3d4d9fea94a874ac7af8c9a0
Leah <leahxharden@gmail.com>
2023-02-03 18:53:03 -0500

    added tags

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
 //api.openweathermap.org / data / 2.5 / forecast ? lat = { 28.5384 } & lon={ 81.3789 }& appid={API key }
 
 
-fetch('api.openweathermap.org/data/2.5/forecast?lat=28.5384& lon=81.3789&appid={28c3589b9ea291ef8351565407890153}&units=imperial', {
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=28.5384&lon=81.3789&appid=28c3589b9ea291ef8351565407890153&units=imperial', {
     method: 'POST'
 })
     .then((response) => response.json())
     .then((data) => {
         console.log('Success:', data);
+
+
+
+
+        
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 
+    list.wind.speed
